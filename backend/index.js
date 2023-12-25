3215336d5b519cc4ffeaa47facdf523455a8916a
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2023-12-25 13:01:58 +0200

    connected to the mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("hi")
+const express = require("express");
+const { connectToMongoDB } = require("./config/mongoDB");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require("dotenv").config();
+
+// Start the server
+app.listen(8000, () => {
+    console.log("Server listening on PORT: ", 8000);
+    connectToMongoDB(process.env.MONGODB_URL); // Pass the MongoDB URL
+});
