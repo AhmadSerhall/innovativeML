f0f1437672b735173711c5deec80093e1a402007
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2023-12-28 18:54:46 +0200

    create workspace api finally working

--- backend/models/workspace.model.js
+++ backend/models/workspace.model.js
@@ -1,71 +1,39 @@
-// const mongoose = require('mongoose');
 
-// const workspaceSchema = new mongoose.Schema({
-//   workspace_data: {
-//     type: {
-//       type: String,
-//       required: true,
-//       enum: ['workspace'],
-//     },
-//     blocks: [
-//       {
-//         id: String,
-//         type: String,
-//         position: {
-//           x: Number,
-//           y: Number,
-//         },
-//         properties: {
-//           name: String,
-//           value: Number,
-//         },
-//         inputs: {
-//           A: String,
-//           B: String,
-//         },
-//       },
-//     ],
-//   },
-//   user_id: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User', 
-//     required: true,
-//   },
-// });
+const mongoose = require('mongoose');
 
-// const Workspace = mongoose.model('Workspace', workspaceSchema);
-
-// module.exports = Workspace;
 const workspaceSchema = new mongoose.Schema({
-    workspace_data: {
-      type: {
-        type: String,
-        required: true,
-        enum: ['workspace'],
-      },
-      blocks: [
-        {
-          id: String,
-          type: String,
-          position: {
-            x: Number,
-            y: Number,
-          },
-          properties: {
-            name: String,
-            value: Number,
-          },
-          inputs: {
-            A: String,
-            B: String,
-          },
-        }
-      ],
-    },
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+  workspace_data: {
+    type: {
+      type: Object,
       required: true,
+      enum: ['workspace'],
     },
-  });
-  
+    blocks: [
+      {
+        id: String,
+        type: Object,
+        position: {
+          x: Number,
+          y: Number,
+        },
+        properties: {
+          name: String,
+          value: Number,
+        },
+        inputs: {
+          A: String,
+          B: String,
+        },
+      },
+    ],
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true,
+  },
+});
+
+const Workspace = mongoose.model('Workspace', workspaceSchema);
+
+module.exports = Workspace;
