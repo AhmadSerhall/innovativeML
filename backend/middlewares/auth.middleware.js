0ead68e5469540d07585bb8e780c844eb577be14
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2023-12-28 15:47:50 +0200

    finished the workspace model

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -14,7 +14,14 @@ const authMiddleware = async (req, res, next) => {
     next();
   }
 };
+const adminMiddleware = (req, res, next) => {
+  if (req.user && req.user.user_type === 'admin') {
+    next(); // Admin is authorized, proceed to the next middleware/route
+  } else {
+    res.status(403).json({ message: 'Forbidden: Admin access required' });
+  }
+};
 
 module.exports = {
-  authMiddleware,
+  authMiddleware,adminMiddleware
 };
--- backend/routes/admin.rout.js
+++ backend/routes/admin.rout.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { getAllUsers, deleteUser } = require('../controllers/admin.controller');
-const { authMiddleware } = require('../middlewares/auth.middleware');
+const { authMiddleware,adminMiddleware} = require('../middlewares/auth.middleware');
 const router = express.Router();
+
+router.use(authMiddleware, adminMiddleware);
 
 router.get('/users', authMiddleware, getAllUsers);
 router.delete('/users/:id', authMiddleware, deleteUser);
