8f4dd025dcab16c4af49ab7cc4a34e1f9fabfb98
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-01 20:55:54 +0200

    back at it 2024 trying to fix fcm

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const User = require("../models/user.model");
 const bcrypt =require("bcrypt");
-const jwt=require("jsonwebtoken")
+const jwt=require("jsonwebtoken");
+const admin=require("firebase-admin");
 //firebase messages
 const login = async (req, res) => {
   const { username, password } = req.body;
