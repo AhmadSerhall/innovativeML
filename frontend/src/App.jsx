8722bde536adc85f0e60fa7d26244af0f377996c
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-04 19:50:14 +0200

    TitleLogo component finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
+import TitleLogo from './components/TitleLogo';
 import './styles/global.css'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      gg
+      <BrowserRouter>
+        <Routes>
+          {/* <Route className="test" path="/test" element={<TitleLogo/>}/> */}
+          
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- frontend/src/components/TitleLogo/index.jsx
+++ frontend/src/components/TitleLogo/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../../styles/global.css'
 import './style.css'
+import InnovativeMLLogo from '../../assets/InnovativeMLLogo.png'
 const TitleLogo = () => {
   return (
-    <div className='titlelogo flex '>
-      <img src="../assets/InnovativeMLLogo.png" alt='logo'/>
+    <div className='title-logo flex row'>
+        <div className='img-container'>
+            <img src={InnovativeMLLogo} alt='logo'/>
+        </div>
       <h1>InnovativeML</h1>
     </div>
   )
