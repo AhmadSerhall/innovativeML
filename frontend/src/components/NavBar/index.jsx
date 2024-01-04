8ab31d936ce021a54eb0eef17700ada313b30375
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-04 20:56:45 +0200

    finished the NavBar component/reusable

--- frontend/src/components/NavBar/index.jsx
+++ frontend/src/components/NavBar/index.jsx
@@ -1,19 +1,62 @@
-import React from 'react'
-import '../../styles/global.css'
-import TitleLogo from '../TitleLogo'
-import './style.css'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/global.css';
+import './style.css'; 
+import TitleLogo from '../TitleLogo';
+
 const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const navLinksRef = useRef(null);//to be able to click anywhere to close the burger
+  const burgerIconRef = useRef(null);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  const closeLinks = () => {
+    setShowLinks(false);
+  };
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+    closeLinks();
+  };
+
+  const handleClickOutside = (event) => {
+    if (
+      (navLinksRef.current && !navLinksRef.current.contains(event.target)) &&
+      (burgerIconRef.current && !burgerIconRef.current.contains(event.target))
+    ) {
+      closeLinks();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className='nav-bar flex row full-width'>
-      <TitleLogo/>
-      <ul className='nav-links flex '>
-        <li><Link to="/Landing">Home</Link></li>
-        <li><Link to="/About">About us</Link></li>
-        <li><Link to="/FAQ's">FAQ's</Link></li>
+    <div className='nav-bar'>
+      <TitleLogo />
+      {(windowWidth <= 768) && (
+        <div ref={burgerIconRef} className="burger-icon" onClick={toggleLinks}>
+          &#9776;
+        </div>
+      )}
+      <ul ref={navLinksRef} className={`nav-links ${showLinks ? 'show' : ''}`}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About us</Link></li>
+        <li><Link to="/faqs">FAQ's</Link></li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
