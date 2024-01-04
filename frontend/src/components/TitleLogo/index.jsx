6ae84682ab893ebe715d3d65ce42da0f75498f2e
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-04 18:29:27 +0200

    starting the first component TitleLogo

---
+++ frontend/src/components/TitleLogo/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../styles/global.css'
+import './style.css'
+const TitleLogo = () => {
+  return (
+    <div className='titlelogo flex '>
+      <img src="../assets/InnovativeMLLogo.png" alt='logo'/>
+      <h1>InnovativeML</h1>
+    </div>
+  )
+}
+
+export default TitleLogo
