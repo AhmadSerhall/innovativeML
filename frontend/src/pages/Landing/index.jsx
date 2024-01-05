1e148c982b80ff8a7d8385124dd9f37bc6e7f38e
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-05 21:24:29 +0200

    added the reusable components needed in the first part of the landing page

--- frontend/src/pages/Landing/index.jsx
+++ frontend/src/pages/Landing/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import NavBar from '../../components/NavBar'
-
+import '../../styles/global.css'
+import Button from '../../components/Button'
 const Landing = () => {
   return (
     <div>
       <NavBar/>
+      <div className='hero-container flex row'>
+        <div className='left-section flex column'>
+            <h1>The best way to learn</h1>
+            <br/>
+            <h1>machine learning</h1>
+            <div className='button-container flex row'>
+                <Button text="Train Machine" textColor="white"/>
+                <Button/>
+            </div>
+        </div>
+
+      </div>
       
     </div>
   )
