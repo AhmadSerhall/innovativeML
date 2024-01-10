ad2a80a8803a9dd48582beab9e654393fef90525
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-10 20:15:41 +0200

    frontend -- created the experice container containing a title and a paragraph

---
+++ frontend/src/components/Experience/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className='experience-container flex column'>
+      <div className='experience-text'>
+        <h2>Navigating the InnovativeML experience</h2>
+        <p>At InnovativeML we created a eye catching user friendly interface that allows Users to either Experience machine learning , or learn coding throughtout a new experience</p>
+      </div>
+    </div>
+  )
+}
+
+export default Experience
