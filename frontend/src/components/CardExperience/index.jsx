e7ab8493202077d115c1b506f6f2225a00aa568e
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-11 20:23:28 +0200

    frontend implemented the top left line on each container part 1

--- frontend/src/components/CardExperience/index.jsx
+++ frontend/src/components/CardExperience/index.jsx
@@ -4,6 +4,7 @@ import './style.css';
 const CardExperience = ({ step, title, text,}) => {
  return (
     <div className="CardExperience">
+        <div className="TopLeftLine"></div>
         <div className="CardExperience__step">{step}</div>
         <h3 className="CardExperience__title">{title}</h3>
         <p className="CardExperience__text">{text}</p>
