15164a7dc6050ef087c1898e19e440e6b33052a1
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-11 19:51:57 +0200

    frontend changed the color in each individual card and added on hover effect

--- frontend/src/components/CardExperience/index.jsx
+++ frontend/src/components/CardExperience/index.jsx
@@ -4,11 +4,9 @@ import './style.css';
 const CardExperience = ({ step, title, text,}) => {
  return (
     <div className="CardExperience">
-      {/* <div className="CardExperience__content"> */}
         <div className="CardExperience__step">{step}</div>
         <h3 className="CardExperience__title">{title}</h3>
         <p className="CardExperience__text">{text}</p>
-      {/* </div> */}
     </div>
  );
 };
