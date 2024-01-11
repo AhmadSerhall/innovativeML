0fb8e5d8a723ae0151a0fd872f370f435893ed1e
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-11 21:27:08 +0200

    changed title for card 4

--- frontend/src/components/Experience/index.jsx
+++ frontend/src/components/Experience/index.jsx
@@ -12,7 +12,7 @@ const Experience = () => {
         <CardExperience step={"Step 01"} title={"Admiring AI"} text={"Dive into the vast world of machine learning. Discover the potential it holds for transforming your understanding of technology."} />
         <CardExperience step={"Step 02"} title={"Engage with Interactive Puzzles"} text={"Immerse yourself in hands-on learning. Solve interactive puzzles that bring machine learning concepts to life, making coding a joyful experience."} />
         <CardExperience step={"Step 03"} title={" Guided Learning Paths"} text={"Follow carefully curated learning paths that guide you through the intricacies of machine learning, ensuring a structured and effective educational journey."} />
-        <CardExperience step={"Step 04"} title={"title1"} text={"this is the first text"} />
+        <CardExperience step={"Step 04"} title={" Master Complex Concepts"} text={"this is the first text"} />
         <CardExperience step={"step_1"} title={"title1"} text={"this is the first text"} />
         <CardExperience step={"step_1"} title={"title1"} text={"this is the first text"} />
 
