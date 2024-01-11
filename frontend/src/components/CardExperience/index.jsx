5dff01cac14ca1e294e328c51acb4e40dda514a3
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-11 20:35:52 +0200

    frontend implemented the wanted design part 2

--- frontend/src/components/CardExperience/index.jsx
+++ frontend/src/components/CardExperience/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './style.css';
 
-const CardExperience = ({ step, title, text,}) => {
- return (
+const CardExperience = ({ step, title, text }) => {
+  return (
     <div className="CardExperience">
-        <div className="TopLeftLine"></div>
+      <div className="TopLeftLine"></div>
+      <div className="StepContainer">
         <div className="CardExperience__step">{step}</div>
+      </div>
+      <div className="CardContent">
         <h3 className="CardExperience__title">{title}</h3>
         <p className="CardExperience__text">{text}</p>
+      </div>
     </div>
- );
+  );
 };
 
 export default CardExperience;
