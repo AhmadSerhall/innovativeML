b71f3dbe65ed9e12eaecbdcf704c9f412be0a0da
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-11 21:03:13 +0200

    frontend changing the step of the card 2

--- frontend/src/components/Experience/index.jsx
+++ frontend/src/components/Experience/index.jsx
@@ -10,7 +10,7 @@ const Experience = () => {
       </div>
       <div className='experience-card-container flex'>
         <CardExperience step={"Step 01"} title={"Admiring AI"} text={"Discovering machine learning and artificial intelligence (AI) opens the door to a new era of technological wonders. Machine learning, a subset of AI, enables computers to learn from experience without explicit programming. It's an exploration of algorithms that decipher patterns, make predictions, and redefine how we interact with the digital world. Delving into machine learning unveils the potential to revolutionize industries and shape the future of intelligent technologies."} />
-        <CardExperience step={"step02"} title={"title1"} text={"this is the first text"} />
+        <CardExperience step={"step 02"} title={"title1"} text={"this is the first text"} />
         <CardExperience step={"step_1"} title={"title1"} text={"this is the first text"} />
         <CardExperience step={"step_1"} title={"title1"} text={"this is the first text"} />
         <CardExperience step={"step_1"} title={"title1"} text={"this is the first text"} />
