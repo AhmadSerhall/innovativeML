12d355175aea5b413da02939a1720b5af3390b16
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-11 21:20:47 +0200

    frontend changed the text in card 1

--- frontend/src/components/Experience/index.jsx
+++ frontend/src/components/Experience/index.jsx
@@ -9,8 +9,8 @@ const Experience = () => {
         <p>At InnovativeML we created a eye catching user friendly interface that allows Users to either Experience machine learning , or learn coding throughtout a new experience</p>
       </div>
       <div className='experience-card-container flex'>
-        <CardExperience step={"Step 01"} title={"Admiring AI"} text={"Discovering machine learning and artificial intelligence (AI) opens the door to a new era of technological wonders. Machine learning, a subset of AI, enables computers to learn from experience without explicit programming. It's an exploration of algorithms that decipher patterns, make predictions, and redefine how we interact with the digital world. Delving into machine learning unveils the potential to revolutionize industries and shape the future of intelligent technologies."} />
-        <CardExperience step={"step 02"} title={"Discovering InnovativeML"} text={"this is the first text"} />
+        <CardExperience step={"Step 01"} title={"Admiring AI"} text={"Dive into the vast world of machine learning. Discover the potential it holds for transforming your understanding of technology."} />
+        <CardExperience step={"step 02"} title={"Discovering InnovativeML"} text={"Embark on a journey of exploration with InnovativeML that makes learning machine learning a captivating adventure. Uncover the world of code creation through engaging puzzles, where every challenge unlocks the secrets of machine learning. Our app seamlessly combines education and entertainment, transforming complex concepts into interactive, bite-sized puzzles. Discover the joy of mastering machine learning as you code your way through a fun and enlightening experience, paving the way for a future filled with technological prowess."} />
         <CardExperience step={"step_1"} title={"title1"} text={"this is the first text"} />
         <CardExperience step={"step_1"} title={"title1"} text={"this is the first text"} />
         <CardExperience step={"step_1"} title={"title1"} text={"this is the first text"} />
