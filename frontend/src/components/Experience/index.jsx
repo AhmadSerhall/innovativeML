32b1e6ab287ae803f21e9355e8544e0f7856ce6d
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-12 23:59:31 +0200

    made the e capital letter in the word experience

--- frontend/src/components/Experience/index.jsx
+++ frontend/src/components/Experience/index.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
   return (
     <div className='experience-container flex column'>
       <div className='experience-text'>
-        <h2>Navigating the InnovativeML experience</h2>
+        <h2>Navigating the InnovativeML Experience</h2>
         <p>At InnovativeML we created a eye catching user friendly interface that allows Users to either Experience machine learning , or learn coding throughtout a new experience</p>
       </div>
       <div className='experience-card-container flex'>
