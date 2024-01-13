cbc243e27388f1bd5ee0ab50bf87306c8224ec66
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-13 11:22:19 +0200

    used the NavBar component in the contact page

--- frontend/src/pages/Contact/index.jsx
+++ frontend/src/pages/Contact/index.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import './style.css'
 import '../../styles/global.css'
 import '../../components/NavBar'
+import NavBar from '../../components/NavBar'
 const Contact = () => {
   return (
     <div>
-      <h1>Contact us</h1>
+      <NavBar/>
     </div>
   )
 }
