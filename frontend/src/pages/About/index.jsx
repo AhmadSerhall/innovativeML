0086991d58436604d135ac417cba5043a0b415b4
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-13 14:16:46 +0200

    adjusting the animation for the about page

--- frontend/src/pages/About/index.jsx
+++ frontend/src/pages/About/index.jsx
@@ -8,21 +8,22 @@ import aboutpic2 from '../../assets/aboutus hero2.webp'
 import Vision from '../../components/Vision'
 import Experience from '../../components/Experience'
 import Footer from '../../components/Footer'
+import '../../styles/animate.css'
 const About = () => {
   return (
     <div>
       <NavBar/>
-      <div className='aboutus-hero flex'>
-        <div className='aboutus-text flex column'>
+      <div className='aboutus-hero flex '>
+        <div className='aboutus-text flex column '>
           <h2>Our Vision</h2>
           <p>At InnovativeML, we believe in transforming the landscape of learning through the fusion of cutting-edge technology and education. Our vision is to empower individuals to master the art of coding and machine learning in an interactive, engaging, and innovative way.</p>
-          <div className='aboutus-containers flex'>
+          <div className='aboutus-containers flex  '>
           <AboutContainers title={"10+"} text={"Users"}/>
           <AboutContainers title={"3"} text={"months of growth"}/>
           <AboutContainers title={"1"} text={"year of experience"}/>
         </div>
         </div>
-        <div className='aboutus-img-container'>
+        <div className='aboutus-img-container  '>
           <img src={aboutpic2} alt="about pic"></img>
         </div>
         
--- frontend/src/pages/Contact/index.jsx
+++ frontend/src/pages/Contact/index.jsx
@@ -28,13 +28,13 @@ const Contact = () => {
     <div>
       <NavBar/>
       <div className='contact-container  flex'>
-        <div className='contact-info flex column animate'>
+        <div className='contact-info flex column '>
           <h1 className='contact-title'>Contact Us</h1>
           <Input name={"username"}  placeholder={"Enter Your Username"} onChange={handleUsernameChange} />
           <Input className ="area"name={"message"}  placeholder={"Enter Your Message"} onChange={handleMessageChange} type="textarea"/>
           <Button text={"Send Message"} bgColor={"#1261A9"} onClick={SendMessage} />
           </div>
-          <div className='contact-img-container animate'>
+          <div className='contact-img-container '>
             <img src={contactimg} alt="contact img"/>
           </div>
       </div>
