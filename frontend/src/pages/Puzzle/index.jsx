39dfb6a7561509afbbb0bb18c2d883f2e89047a5
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-14 19:49:57 +0200

    created a workspace const that inject blockly and add Blockly puzzles in my puzzle page in my index.jsx

--- frontend/src/pages/Puzzle/index.jsx
+++ frontend/src/pages/Puzzle/index.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Blockly from 'blockly'
 import 'blockly/blocks'
 const Puzzle = () => {
+    useEffect(()=>{
+        const workspace=Blockly.inject('blocklyDiv',{
+            toolbox:document.getElementById('toolbox')
+        })
+    })
   return (
     <div>
         <h1>Puzzle page</h1>
