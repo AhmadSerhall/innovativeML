f4cdffec6230e7dd9ebcd2307b337a22f1b18a81
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-14 22:30:59 +0200

    fixed issue of block container duplicating once file updated part 1

--- frontend/src/pages/Puzzle/index.jsx
+++ frontend/src/pages/Puzzle/index.jsx
@@ -1,27 +1,47 @@
-import React, { useEffect } from 'react'
-import Blockly from 'blockly'
-import 'blockly/blocks'
-import './style.css'
+import React, { useEffect } from 'react';
+import Blockly from 'blockly';
+import 'blockly/blocks';
+// import 'blockly/themes/classic';
+
 const Puzzle = () => {
-    useEffect(()=>{
-        const workspace=Blockly.inject('blocklyDiv',{
-            toolbox:document.getElementById('toolbox')
-        })
-        const block=workspace.newBlock('text_print');
-        block.initSvg()
-        block.render()
-    },[])
+  useEffect(() => {
+    const toolbox = document.getElementById('toolbox');
+    const blocklyDiv = document.getElementById('blocklyDiv');
 
-    
+    // Cleanup Blockly workspace if it already exists
+    if (blocklyDiv && blocklyDiv.firstChild) {
+      blocklyDiv.removeChild(blocklyDiv.firstChild);
+    }
+
+    // Check if Blockly containers are already initialized
+    if (!toolbox && !blocklyDiv) {
+      // Initialize Blockly
+      const workspace = Blockly.inject(blocklyDiv, {
+        toolbox: toolbox,
+      });
+
+      // Create and render a block in the workspace
+      const block = workspace.newBlock('text_print');
+      block.initSvg();
+      block.render();
+    }
+
+    // Cleanup Blockly workspace on component unmount
+    return () => {
+      if (blocklyDiv) {
+        blocklyDiv.removeChild(blocklyDiv.firstChild);
+      }
+    };
+  }, []);
+
   return (
-    <div id="blocklyDiv"style={{ height: '480px', width: '600px' }} className='puzzle-container'>
-        <xml> {/* used extensible markup language --sets of codes,tags,  */}
-        <block type="text" ></block>
-        
-        </xml>
-        
+    <div className='puzzle-container'>
+      <xml id='toolbox' style={{ display: 'none' }}>
+        <block type='text'></block>
+      </xml>
+      <div id='blocklyDiv' style={{ height: '480px', width: '600px' }}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Puzzle
+export default Puzzle;
