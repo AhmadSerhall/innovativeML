9dadede10cdf69a65ba51c28c8b1c33cf70627f3
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-15 19:33:46 +0200

    changed the link and script tags in my index.html in my public folder to fix the problem i am facing

--- frontend/src/pages/Puzzle/index.jsx
+++ frontend/src/pages/Puzzle/index.jsx
@@ -1,44 +1,28 @@
 import React, { useEffect } from 'react';
 import Blockly from 'blockly';
 import 'blockly/blocks';
-// import 'blockly/themes/classic';
+import './style.css';
 
 const Puzzle = () => {
   useEffect(() => {
+    // Load the toolbox once when the component mounts
     const toolbox = document.getElementById('toolbox');
-    const blocklyDiv = document.getElementById('blocklyDiv');
+    const workspace = Blockly.inject('blocklyDiv', {
+      toolbox,
+    });
 
-    // Cleanup Blockly workspace if it already exists
-    if (blocklyDiv && blocklyDiv.firstChild) {
-      blocklyDiv.removeChild(blocklyDiv.firstChild);
-    }
-
-    // Check if Blockly containers are already initialized
-    if (!toolbox && !blocklyDiv) {
-      // Initialize Blockly
-      const workspace = Blockly.inject(blocklyDiv, {
-        toolbox: toolbox,
-      });
-
-      // Create and render a block in the workspace
-      const block = workspace.newBlock('text_print');
-      block.initSvg();
-      block.render();
-    }
-
-    // Cleanup Blockly workspace on component unmount
-    return () => {
-      if (blocklyDiv) {
-        blocklyDiv.removeChild(blocklyDiv.firstChild);
-      }
-    };
+    const block = workspace.newBlock('text_print');
+    block.initSvg();
+    block.render();
   }, []);
 
   return (
-    <div className='puzzle-container'>
+    <div>
       <xml id='toolbox' style={{ display: 'none' }}>
         <block type='text'></block>
       </xml>
+
+
       <div id='blocklyDiv' style={{ height: '480px', width: '600px' }}></div>
     </div>
   );
