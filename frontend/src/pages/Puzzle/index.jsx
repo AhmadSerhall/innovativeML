f11beff1efa27a8502cce3fdba5a95cc6d4ff214
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-16 09:45:40 +0200

    added the custom integer block

--- frontend/src/pages/Puzzle/index.jsx
+++ frontend/src/pages/Puzzle/index.jsx
@@ -3,6 +3,17 @@ import Blockly from 'blockly';
 import 'blockly/blocks';
 import './style.css';
 
+// Register custom block types
+Blockly.Blocks['type_integer'] = {
+  init: function () {
+    this.appendDummyInput().appendField(new Blockly.FieldNumber(0), 'NAME');
+    this.setOutput(true, 'Number');
+    this.setColour(230);
+    this.setTooltip('This is a custom integer block');
+    this.setHelpUrl('');
+  },
+};
+
 const Puzzle = () => {
   useEffect(() => {
     const toolbox = document.getElementById('toolbox');
@@ -19,6 +30,7 @@ const Puzzle = () => {
       console.error('Blockly workspace not initialized!');
       return;
     }
+
     const block = workspace.newBlock('text_print');
     block.initSvg();
     block.render();
@@ -36,7 +48,7 @@ const Puzzle = () => {
         <category name="Text" colour="#2196F3">
           <block type="text_print"></block>
           <block type='text'></block>
-          <block type="type_integer"></block>
+          <block type='type_integer'></block> 
         </category>
       </xml>
 
@@ -44,4 +56,5 @@ const Puzzle = () => {
     </div>
   );
 };
+
 export default Puzzle;
