f86fdb640552d44179b96d088dac450da457ebb1
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-17 15:16:26 +0200

    wrote the code for the DoIf block inside the logic folder in the index.jsx file

--- frontend/src/components/PuzzleBlocks/Logic/index.jsx
+++ frontend/src/components/PuzzleBlocks/Logic/index.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React from 'react';
 import Blockly from 'blockly';
-import 'blockly/blocks';
+import { useEffect } from 'react';
+
+Blockly.Blocks['do_if'] = {
+  init: function() {
+    this.jsonInit({
+      "message0": "do if %1",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "CONDITION",
+          "check": "Boolean"
+        }
+      ],
+      "colour": "#5BA58C",
+      "previousStatement": null,
+      "nextStatement": null,
+      "tooltip": "Perform an action if a condition is true.",
+      "helpUrl": ""
+    });
+  }
+};
 
 const Logic = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  useEffect(() => {
+    const block = new Blockly.Block('do_if');
+    block.initSvg();
+    block.render();
+  }, []);
 
-export default Logic
+  return (
+    <div id="doIfBlock" style={{ display: 'none' }}>
+      {/* Inject the block into the page */}
+    </div>
+  );
+};
+
+export default Logic;
