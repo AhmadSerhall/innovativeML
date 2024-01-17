c89f6337533972dccfc462019476ddead569be46
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-17 15:04:47 +0200

    initialized the Logic category and imported blockly

---
+++ frontend/src/components/PuzzleBlocks/Logic/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Blockly from 'blockly';
+import 'blockly/blocks';
+
+const Logic = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Logic
