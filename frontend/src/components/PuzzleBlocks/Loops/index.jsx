0ed2d0cd67c0bd89f2e61150f68b7f2fa8c29f63
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-18 00:34:46 +0200

    created the function for block repeat_times_do block inside the Loops folder inside the index.jsx

--- frontend/src/components/PuzzleBlocks/Loops/index.jsx
+++ frontend/src/components/PuzzleBlocks/Loops/index.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 import Blockly from 'blockly';
 import { useEffect } from 'react';
 const Loops = () => {
+    Blockly.Blocks['repeat_times_do'] = {
+        init: function () {
+          this.appendValueInput('TIMES')
+            .setCheck('Number')
+            .appendField('repeat');
+          this.appendStatementInput('DO')
+            .setCheck(null)
+            .appendField('do');
+          this.setPreviousStatement(true, null);
+          this.setNextStatement(true, null);
+          this.setColour(120);
+          this.setTooltip('Repeat a block of code a certain number of times');
+          this.setHelpUrl('');
+        },
+      };
   return (
     <div>
       
