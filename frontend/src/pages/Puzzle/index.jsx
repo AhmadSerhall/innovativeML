37142b66dfcfd67f30ac30df3cdeeb80c0c01a00
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-18 03:14:08 +0200

    included the random_integer block in the math category inside the puzzle page

--- frontend/src/pages/Puzzle/index.jsx
+++ frontend/src/pages/Puzzle/index.jsx
@@ -198,6 +198,8 @@ const Puzzle = () => {
           <block type='math_trigo'></block>
           <block type='check_number_property'></block> 
           <block type='list_operations'></block> 
+          <block type='random_integer'></block> 
+
 
  
  
