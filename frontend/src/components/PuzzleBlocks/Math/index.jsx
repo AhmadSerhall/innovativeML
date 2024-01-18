a1ba5a3bd7ef9030562c317e75bd6f47032c42d2
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-18 02:26:33 +0200

    created the math_operations block

--- frontend/src/components/PuzzleBlocks/Math/index.jsx
+++ frontend/src/components/PuzzleBlocks/Math/index.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 import Blockly from 'blockly';
 import { useEffect } from 'react';
+
+Blockly.Blocks['math_operations'] = {
+    init: function () {
+      this.appendValueInput("LEFT")
+        .setCheck("Number")
+        .appendField("");
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([
+          ["+", "ADD"],
+          ["-", "SUBTRACT"],
+          ["*", "MULTIPLY"],
+          ["/", "DIVIDE"]
+        ]), "OPERATOR");
+      this.appendValueInput("RIGHT")
+        .setCheck("Number")
+        .appendField("");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour("#FF9933"); // Orange color for math operations
+      this.setTooltip("Perform a math operation");
+      this.setHelpUrl("");
+    }
+  };
+  
 const Math = () => {
   return (
     <div>
