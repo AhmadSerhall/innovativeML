82b39063e6652aa72aeb2da8d96fe900a40f4d8a
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-19 15:46:24 +0200

    created the useEffect hook for the lists_getIndexOfItem block'

--- frontend/src/components/PuzzleBlocks/List/index.jsx
+++ frontend/src/components/PuzzleBlocks/List/index.jsx
@@ -174,7 +174,11 @@ const List = () => {
     block.initSvg();
     block.render();
   }, []);
-
+  useEffect(() => {
+    const block = new Blockly.Block('lists_getIndexOfItem');
+    block.initSvg();
+    block.render();
+  }, []);
   return (
     <div>
       <div id="lists_create_with_item" style={{ display: 'none' }}></div>
