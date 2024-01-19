23f6e8d0f5e5f1b4b36073d35beb19ed4415baec
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-01-20 00:27:09 +0200

    added classname for python button to utilize useState hook

--- frontend/src/pages/Puzzle/index.jsx
+++ frontend/src/pages/Puzzle/index.jsx
@@ -53,7 +53,7 @@ const Puzzle = () => {
       <NavBar/>
       <div className='tab-container flex row '>
         <Button text="puzzle" textColor={"white"} bgColor={"#1261A9"} onClick={handleTabChange} className={selectedTab === 'puzzle' ? 'active-tab' : 'inactive-tab'}/>
-        <Button text="Python code" textColor={"white"} bgColor={"#FFD700"} onClick={handleTabChange}/>
+        <Button text="Python code" textColor={"white"} bgColor={"#FFD700"} onClick={handleTabChange} className={selectedTab === 'puzzle' ? 'active-tab' : 'inactive-tab'}/>
 
       </div>
     <div className='puzzle-container'>
